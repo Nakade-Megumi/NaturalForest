aa2fe3d9faf782c901117e79f72ce8a6c893afed
mayumoco <134895114+mayumoco@users.noreply.github.com>
2024-01-05 15:51:31 +0900

    ベース

---
+++ Assets/Scenes/collection_flower.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript_collection_flower : MonoBehaviour
+{
+    public void OnClickStartButton_collection_flower()
+    {
+        SceneManager.LoadScene("Collection_flower");
+    }   
+}
---
+++ Assets/Scenes/zukan_ni_modoruyo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript_zukan_ni_modorunoda : MonoBehaviour
+{
+    public void OnClickStartButton_zukan_ni_modorunoda()
+    {
+        SceneManager.LoadScene("Collection");
+    }   
+}
