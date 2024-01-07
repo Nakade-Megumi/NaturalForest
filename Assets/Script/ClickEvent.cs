1aad08a63bc70c72c3aefed9c427fa853abcf97b
Nakade-Megumi <m1m23a21@st.oit.ac.jp>
2024-01-07 22:28:41 +0900

    copy miss ClickEvent.cs

--- Assets/Script/ClickEvent.cs
+++ Assets/Script/ClickEvent.cs
@@ -1,49 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-//マップ上の要素をクリックしたら，特定のイベントが発生する
-public class ClickEvent : MonoBehaviour
+
+public class StartEvent : MonoBehaviour
 {
-    int click;
-
-    public void OnFlower()
+    public InitGame GetInit()
     {
-        Debug.Log("click_event_flower");
-        click = 0;
-        StartEvent startEvent = gameObject.AddComponent<StartEvent>();
-        startEvent.SearchEvent(click);
+        InitGame init;
+        GameObject obj = GameObject.Find("EventSystem");
+        init = obj.GetComponent<InitGame>();
+        return init;
     }
 
-    public void OnTree()
+    public void ShowInit()
     {
-        Debug.Log("click_event_tree");
-        click = 1;
-        StartEvent startEvent = gameObject.AddComponent<StartEvent>();
-        startEvent.SearchEvent(click);
+        InitGame initGame = GetInit();
+        initGame.img.texture = initGame.texture_list_init[0];
     }
 
-    public void OnRiver()
+    public void SearchEvent(int click)
     {
-        Debug.Log("click_event_river");
-        click = 2;
-        StartEvent startEvent = gameObject.AddComponent<StartEvent>();
-        startEvent.SearchEvent(click);
+        switch (click)
+        {
+            case 0:
+                EventFlower();
+                break;
+            case 1:
+                EventTree().Forget();
+                break;
+            case 2:
+                EventRiver();
+                break;
+            case 3:
+                EventSea();
+                break;
+            default:
+                break;
+        }
     }
 
-    public void OnSea()
+    public void EventFlower()
     {
-        Debug.Log("click_event_sea");
-        click = 3;
-        StartEvent startEvent = gameObject.AddComponent<StartEvent>();
-        startEvent.SearchEvent(click);
+        InitGame initGame_flower = GetInit();
+        initGame_flower.img.texture = initGame_flower.texture_list_flower[0];
+    }
+
+    public async UniTask EventTree()
+    {
+        InitGame initGame_tree = GetInit();
+        initGame_tree.img.texture = initGame_tree.texture_list_tree[0];
+        await UniTask.Delay(2000);
+        JudgeTreeEvent(initGame_tree);
+    }
+
+    public void EventRiver()
+    {
+        InitGame initGame_river = GetInit();
+        initGame_river.img.texture = initGame_river.texture_list_river[0];
 
     }
 
-    public void OnHouse()
+    public void EventSea()
     {
-        Debug.Log("click_event_house");
+        InitGame initGame_sea = GetInit();
+        initGame_sea.img.texture = initGame_sea.texture_list_sea[0];
+
     }
 
+    public void JudgeTreeEvent(InitGame initGame)
+    {
+        int judge = Random.Range(0, 10);
+        int card_num = Random.Range(1, 4);
+
+        Debug.Log(judge);
+
+        if (judge == 0 || judge == 10)
+        {
+            initGame.img.texture = initGame.texture_list_tree[7];
+            Debug.Log("気合");
+        }
+        else if (judge % 2 == 0)
+        {
+            initGame.img.texture = initGame.texture_list_tree[card_num];
+            Debug.Log("Success!");
+            Debug.Log(card_num);
+        }
+        else
+        {
+            initGame.img.texture = initGame.texture_list_tree[6];
+            Debug.Log("Failed...");
+        }
+        Invoke("ShowInit", 3);
+    }
 }
