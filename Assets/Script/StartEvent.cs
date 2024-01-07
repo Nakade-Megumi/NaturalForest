5981197a4f440af2885aba12f5f85a6876d47980
Nakade-Megumi <m1m23a21@st.oit.ac.jp>
2024-01-07 17:34:55 +0900

    change startEvent

--- Assets/Script/StartEvent.cs
+++ Assets/Script/StartEvent.cs
@@ -5,10 +5,22 @@ using UnityEngine.UI;
 
 public class StartEvent : MonoBehaviour
 {
+    public InitGame GetInit()
+    {
+        InitGame init;
+        GameObject obj = GameObject.Find("EventSystem");
+        init = obj.GetComponent<InitGame>();
+        return init;
+    }
+
+    public void ShowInit()
+    {
+        InitGame initGame = GetInit();
+        initGame.img.texture = initGame.texture_list_init[0];
+    }
+
     public void SearchEvent(int click)
     {
-        //InitGame initGame;
-        //GameObject obj = GameObject.Find("EventSystem");
         switch (click)
         {
             case 0:
@@ -30,40 +42,44 @@ public class StartEvent : MonoBehaviour
 
     public void EventFlower()
     {
-
+        InitGame initGame_flower = GetInit();
+        initGame_flower.img.texture = initGame_flower.texture_list_flower[0];
     }
 
     public void EventTree()
     {
-        InitGame initGame;
-        GameObject obj = GameObject.Find("EventSystem");
-        initGame = obj.GetComponent<InitGame>();
-
-        initGame.img.texture = initGame.texture_list_tree[0];
-        Invoke("JudgeFruitEvent",2);
+        InitGame initGame_tree = GetInit();
+        initGame_tree.img.texture = initGame_tree.texture_list_tree[0];
+        Invoke("JudgeTreeEvent",2);
     }
     
     public void EventRiver()
     {
+        InitGame initGame_river = GetInit();
+        initGame_river.img.texture = initGame_river.texture_list_river[0];
 
     }
 
     public void EventSea()
     {
+        InitGame initGame_sea = GetInit();
+        initGame_sea.img.texture = initGame_sea.texture_list_sea[0];
 
     }
 
-    public void JudgeFruitEvent() {
-        InitGame initGame;
-        GameObject obj = GameObject.Find("EventSystem");
+    public void JudgeTreeEvent() {
+        InitGame initGame = GetInit();
         int judge = Random.Range(0, 10);
         int card_num = Random.Range(1,4);
 
-        initGame = obj.GetComponent<InitGame>();
-
         Debug.Log(judge);
 
-        if (judge % 2 == 0)
+        if(judge == 0 || judge == 10) 
+        {
+            initGame.img.texture = initGame.texture_list_tree[7];
+            Debug.Log("気合");
+        }
+        else if (judge % 2 == 0)
         {
             initGame.img.texture = initGame.texture_list_tree[card_num];
             Debug.Log("Success!");
@@ -75,13 +91,5 @@ public class StartEvent : MonoBehaviour
             Debug.Log("Failed...");
         }
         Invoke("ShowInit", 3);
-    }
-
-    public void ShowInit()
-    {
-        InitGame initGame;
-        GameObject obj = GameObject.Find("EventSystem");
-        initGame = obj.GetComponent<InitGame>();
-        initGame.img.texture = initGame.texture_list_init[0];
     }
 }
